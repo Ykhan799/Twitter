fb388849a1cd87f843e6679fe4b53e9eb3d0fd91
Yaseen Khan <kyaseen49@yahoo.com>
2022-01-27 12:45:17 -0800

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
@@ -0,0 +1,36 @@
+package com.codepath.apps.restclienttemplate.models
+
+import org.json.JSONArray
+import org.json.JSONObject
+
+// Tweet model to extract JSON data
+class Tweet {
+
+    // Define class variables
+    var body: String = ""
+    var createdAt: String = ""
+    var user: User? = null
+
+    // Gets the JSON data for tweets
+    companion object {
+        // returns a tweet object
+        fun fromJson(jsonObject: JSONObject): Tweet {
+            val tweet = Tweet()
+            tweet.body = jsonObject.getString("text")
+            tweet.createdAt = jsonObject.getString("created_at")
+
+            // Gets the user data
+            tweet.user = User.fromJson(jsonObject.getJSONObject("user"))
+            return tweet
+        }
+
+        // Converts jsonArray into an ArrayList
+        fun fromJsonArray(jsonArray: JSONArray): List<Tweet> {
+            val tweets = ArrayList<Tweet>()
+            for (i in 0 until jsonArray.length()) {
+                tweets.add(fromJson(jsonArray.getJSONObject(i)))
+            }
+            return tweets
+        }
+    }
+}
